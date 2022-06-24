708be6f9249abe90288d115605072164ae9d1d27
Polina Radahuz <polina.radahuz@gmail.com>
2022-06-24 10:40:21 +0200

    еще не конец

--- gulpfile.js
+++ gulpfile.js
@@ -30,25 +30,25 @@ const PLUGINS = [
 ];
 
 function scss() {
-  return src(PATH.scssFile).
-    pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)).
-    pipe(postcss(PLUGINS)).
-    pipe(dest(PATH.cssFolder)).
-    pipe(browserSync.stream());
+  return src(PATH.scssFile)
+      .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+      .pipe(postcss(PLUGINS))
+      .pipe(dest(PATH.cssFolder))
+      .pipe(browserSync.stream());
 }
 
 function scssDev() {
-  return src(PATH.scssFile, {sourcemaps: true}).
-    pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)).
-    pipe(postcss(PLUGINS)).
-    pipe(dest(PATH.cssFolder, {sourcemaps: true})).
-    pipe(browserSync.stream());
+  return src(PATH.scssFile, {sourcemaps: true})
+      .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+      .pipe(postcss([mqpacker({sort: sortCSSmq})]))
+      .pipe(dest(PATH.cssFolder, {sourcemaps: true}))
+      .pipe(browserSync.stream());
 }
 
 function comb() {
-  return src(PATH.scssFiles).
-    pipe(csscomb()).
-    pipe(dest(PATH.scssFolder));
+  return src(PATH.scssFiles)
+      .pipe(csscomb())
+      .pipe(dest(PATH.scssFolder));
 }
 
 function syncInit() {
@@ -75,7 +75,6 @@ function watchDevFiles() {
   watch(PATH.scssFiles, series(scssDev));
   watch(PATH.htmlFiles, sync);
   watch(PATH.jsFiles, sync);
-
 }
 
 task('comb', series(comb));
